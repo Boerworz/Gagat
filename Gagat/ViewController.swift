81d13c3f91374bbd8eb986cc2fcc111db2f065e8
Tim Andersson <timandersson@Tims-MacBook-Pro.local>
2017-02-17 19:03:19 +0100

    Initial content commit

---
+++ Gagat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gagat
+//
+//  Created by Tim Andersson on 2017-02-17.
+//  Copyright © 2017 Cocoabeans Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
