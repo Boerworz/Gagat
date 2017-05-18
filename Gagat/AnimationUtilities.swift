d9d2bcb814099928723995e79ff446027233830c
Tim Andersson <tim@ndersson.me>
2017-05-18 21:15:20 +0200

    Limit maximum transition duration based on distance
    This prevents the transition snapshot from moving slower than a certain
    velocity (300 points per second), which makes the transition feel nicer
    when the snapshot only needs to move a small distance (such as when
    cancelling the transition with little to no velocity).

--- Gagat/AnimationUtilities.swift
+++ Gagat/AnimationUtilities.swift
@@ -11,11 +11,21 @@ import QuartzCore
 
 // MARK: - Animation utilities
 
-func timeRequiredToMove(from: CGPoint, to: CGPoint, withVelocity velocity: CGPoint) -> TimeInterval {
-	let distanceToMove = sqrt(pow(to.x - from.x, 2) + pow(to.y - from.y, 2))
+private func distance(from source: CGPoint, to destination: CGPoint) -> CGFloat {
+	return sqrt(pow(destination.x - source.x, 2) + pow(destination.y - source.y, 2))
+}
+
+private func timeRequiredToMove(from source: CGPoint, to destination: CGPoint, withVelocity velocity: CGPoint) -> TimeInterval {
+	let distanceToMove = distance(from: source, to: destination)
 	let velocityMagnitude = sqrt(pow(velocity.x, 2) + pow(velocity.y, 2))
 	let requiredTime = TimeInterval(abs(distanceToMove / velocityMagnitude))
 	return requiredTime
+}
+
+private func maximumReasonableTimeToMove(from source: CGPoint, to destination: CGPoint) -> TimeInterval {
+	let minimumReasonableVelocity: CGFloat = 300.0 // points per second, chosen empirically
+	let distanceToMove = distance(from: source, to: destination)
+	return TimeInterval(distanceToMove / minimumReasonableVelocity)
 }
 
 func animate(_ layer: CALayer, to targetPoint: CGPoint, withVelocity velocity: CGPoint, completion: @escaping () -> Void) {
@@ -23,7 +33,10 @@ func animate(_ layer: CALayer, to targetPoint: CGPoint, withVelocity velocity: C
 	layer.position = targetPoint
 	
 	let positionAnimation = CABasicAnimation(keyPath: "position")
-	positionAnimation.duration = min(3.0, timeRequiredToMove(from: startPoint, to: targetPoint, withVelocity: velocity))
+	positionAnimation.duration = min(
+		maximumReasonableTimeToMove(from: startPoint, to: targetPoint),
+		timeRequiredToMove(from: startPoint, to: targetPoint, withVelocity: velocity)
+	)
 	positionAnimation.fromValue = NSValue(cgPoint: startPoint)
 	positionAnimation.toValue = NSValue(cgPoint: targetPoint)
 	
